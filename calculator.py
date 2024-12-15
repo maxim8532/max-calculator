ed172c63b2aa38df179747bbdd68b48099691ce2
max <muller.max06@gmail.com>
2024-12-15 13:46:39 +0200

    Add method to mark minuses - seperated logic from reduce minuses

--- calculator.py
+++ calculator.py
@@ -4,16 +4,19 @@ from binary_operator import BinaryOperator
 
 
 class Calculator:
-    def __init__(self):
-        ...
+    def __init__(self, expression):
+        self._expression = expression
 
-    def preprocessor(self, expression):
-        expression = expression.replace("", " ")  # Remove whitespaces
+    def preprocessor(self):
+        expression = self._expression.replace(" ", "")  # Remove whitespaces
+        expression = self.reduce_minuses(expression)
+        expression = self.mark_special_minuses(expression)
+        return expression
+
     @staticmethod
     def reduce_minuses(expression):
         """
-        Reduces sequences of minus signs in an expression, distinguishing
-        unary, binary, and sign minuses.
+        Reduces sequences of minus signs in an expression.
 
         :param expression: The mathematical expression
         :type expression: str
@@ -25,33 +28,44 @@ class Calculator:
 
         reduced_expression = ""
         is_minus_sequence = False  # Tracks if we are in a sequence of minuses
-        is_sign_minus = False  # Tracks if the minus is a sign minus
         current_sign_negative = True  # Tracks the current sign, toggles between True/False
 
         for index, char in enumerate(expression):
             if not is_minus_sequence:
-                if char == "-" and (index == 0 or expression[index - 1] == "("):  # Unary minus
+                if char == "-" and (index == 0 or expression[index - 1] == "("
+                                    or Operator.is_valid_operator(expression[index - 1])):  # Start of sequence
                     is_minus_sequence = True
-                elif char == "-" and Operator.is_valid_operator(expression[index - 1]):  # Sign minus
-                    is_minus_sequence = True
-                    is_sign_minus = True
                 else:
-                    reduced_expression += char  # Add non-minus characters
+                    reduced_expression += char
             else:
                 if char == "-":  # Extend minus sequence
                     current_sign_negative = not current_sign_negative  # Toggle sign
                 else:
                     # End of minus sequence
                     if current_sign_negative:
-                        if is_sign_minus:
-                            reduced_expression += "s" + char  # Add sign minus
-                            is_sign_minus = False  # Reset
-                        else:
-                            reduced_expression += "u" + char  # Add unary minus
+                        reduced_expression += "-" + char  # Add unary/sign minus
                     else:
                         reduced_expression += char  # Positive sign, add character with no minuses
                     is_minus_sequence = False  # Reset sequence tracking
 
         return reduced_expression
 
+    @staticmethod
+    def mark_special_minuses(expression):
+        """
+        Marks the unary and sign minuses.
+        :param expression: The expression after the minuses have been reduced
+        :type expression: str
+        :return: The expression with the special minuses marked
+        :rtype: str
+        """
+        marked_expression = ""
+        for index, char in enumerate(expression):
+            if char == "-" and (index == 0 or expression[index - 1] == "("):  # Unary minus
+                marked_expression += "u"
+            elif char == "-" and Operator.is_valid_operator(expression[index - 1]):  # Sign minus
+                marked_expression += "s"
+            else:
+                marked_expression += char
 
+        return marked_expression
