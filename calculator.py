5a0333a5149fd1e64dd3ad1d83e56af86cca9084
max <muller.max06@gmail.com>
2024-12-16 14:56:51 +0200

    Add method to check if expression contains only valid characters

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+from invalid_character_exception import InvalidCharacterException
 from operators import Operator
 from unary_operator import UnaryOperator
 from binary_operator import BinaryOperator
@@ -8,10 +9,45 @@ class Calculator:
         self._expression = expression
 
     def preprocessor(self):
-        expression = self._expression.replace(" ", "")  # Remove whitespaces
-        expression = self.reduce_minuses(expression)
-        expression = self.mark_special_minuses(expression)
-        return expression
+        expression = self._expression
+        try:
+            expression = self._expression.replace(" ", "")  # Remove whitespaces
+            expression = self.only_valid_characters_check(expression)
+            expression = self.reduce_minuses(expression)
+            expression = self.mark_special_minuses(expression)
+            return expression
+        except InvalidCharacterException as e:
+            print(f"{e} \n\033[92mValid Characters:\033[0m numbers (Integer and decimal), brackets () and operators: "
+                  f"{list(Operator.get_operators_keys())}")
+
+    @staticmethod
+    def only_valid_characters_check(expression):
+        """
+        Checks if all the characters in the expression are valid for a mathematical expression.
+        Highlights the invalid characters and raises an exception with details.
+        :param expression: The expression before checking
+        :type expression: str
+        :return: The expression after being checked
+        :rtype: str
+        :raise InvalidCharacterException: if invalid characters appear in the expression
+        """
+        valid_special_characters = "()."
+        expression_after_check = ""
+        invalid_characters_list = []
+
+        for char in expression:
+            if not (char.isnumeric() or Operator.is_valid_operator(char) or char in valid_special_characters):
+                if char not in invalid_characters_list:
+                    invalid_characters_list.append(char)
+                expression_after_check += "\033[91m\033[1m" + char + "\033[0m"
+                # Turns the invalid characters red and bold
+            else:
+                expression_after_check += char  # Adding a valid char
+        if invalid_characters_list:
+            raise InvalidCharacterException(f"{expression_after_check}\n\033[91mInvalid Characters: \033[0m"
+                                            f"{invalid_characters_list}")  # Turns the message sent to the exception red
+        else:
+            return expression_after_check
 
     @staticmethod
     def reduce_minuses(expression):
