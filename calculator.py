a0e6109c0df54d3f052888f43a1b037f88ba6fd6
max <muller.max06@gmail.com>
2024-12-20 00:19:39 +0200

    Add validation step

--- calculator.py
+++ calculator.py
@@ -3,6 +3,7 @@ from invalid_decimal_point_exception import InvalidDecimalPointException
 from operators import Operator
 import preprocessor_utils
 import tokenization_utils
+import validation_utils
 from unary_operator import UnaryOperator
 from binary_operator import BinaryOperator
 
@@ -35,7 +36,7 @@ class Calculator:
         except InvalidCharacterException as e:
             print(f"{e} \n\033[92mValid Characters:\033[0m numbers (Integer and decimal), brackets () and operators: "
                   f"{list(Operator.get_operators_keys())}")
-            self._expression = None
+            self._expression = ""
             # Prints the valid characters explanation in green
 
     def tokenization(self):
@@ -71,7 +72,7 @@ class Calculator:
                         if not next_char.isdecimal():
                             token_list.append(float(current_token))
                             current_token = ""
-                elif char == "s":  # Handle sign minus
+                elif char == "s":  # Handle sign minus  FIXME: missing check if next operand is decimal or dot
                     current_token += "-"
                 else:
                     current_token += char
@@ -81,6 +82,58 @@ class Calculator:
             # Add any remaining token at the end
             if current_token:
                 token_list.append(float(current_token))
+            self._expression = token_list
             return token_list
         except InvalidDecimalPointException as e:
             print(f"{e} \n\033[92mValid Placement of Decimal Point: \033[0m(0).123, 1.23, 123.(0)")
+            self._expression = []  # Expression is not valid
+
+    def validation(self):
+        """
+        A validation step to catch errors that don't follow the rules that should be in every valid expression.
+        The validation step highlights the errors and presents them to the user if needed.
+
+        The steps in the validation process:
+        1. Check for balanced parentheses
+        2. Check for empty parentheses
+        3. Check binary operator placement
+        4. Check negation operator placement
+        5. Check for stand-alone unary operators
+
+        :return: The error message showing the user every error highlighted in red
+        :rtype: str
+        """
+        error_message = ""
+
+        # 1. Check for balanced parentheses
+        check_passed, error_expression = validation_utils.parentheses_check(self._expression)
+        if not check_passed:
+            error_message += f"\n{error_expression}\nParentheses are not balanced."
+
+        # 2. Check for empty parentheses
+        check_passed, error_expression = validation_utils.empty_parentheses_check(self._expression)
+        if not check_passed:
+            error_message += f"\n{error_expression}\nEmpty parentheses are not allowed."
+
+        # 3. Check binary operator placement
+        check_passed, error_expression = validation_utils.binary_operators_between_valid_operands_check(
+            self._expression)
+        if not check_passed:
+            error_message += f"\n{error_expression}\nBinary operators should be between two operands or expressions."
+
+        # 4. Check negation operator placement
+        check_passed, error_expression = validation_utils.negation_operator_next_to_number_check(self._expression)
+        if not check_passed:
+            error_message += f"\n{error_expression}\nNegation operator (~) should be next to a number on its right."
+
+        # 5. Check for stand-alone unary operators
+        check_passed, error_expression = validation_utils.stand_alone_unary_operators_check(self._expression)
+        if not check_passed:
+            error_message += f"\n{error_expression}\nUnary operators cannot be stand-alone."
+
+        # Return errors if any were found
+        if error_message:
+            return error_message
+        else:
+            return self._expression
+
