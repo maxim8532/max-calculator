958f98c086a7f2df9fb3e9bbb9c9994511e25eb3
max <muller.max06@gmail.com>
2024-12-22 20:48:17 +0200

    Add missing docstring in invalid_character_excpetion

--- invalid_character_exception.py
+++ invalid_character_exception.py
@@ -1,4 +1,12 @@
 class InvalidCharacterException(Exception):
+    """
+        Custom exception for characters that are invalid.
+    """
     def __init__(self, message):
+        """
+        Initialize the exception with a problematic expression message.
+
+         :param message: The error message to display.
+        """
         self.message = message
         super().__init__(message)
